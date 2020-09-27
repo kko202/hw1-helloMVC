0397c86d014ff8f00789c7c02f9f3189c8029049
kko202 <sicoky9318@naver.com>
2020-09-27 17:04:11 +0900

    Hw1 - helloMVC

---
+++ src/controller/DoRegister.java
@@ -0,0 +1,66 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Customer;
+import service.CustomerService;
+
+/**
+ * Servlet implementation class DoRegister
+ */
+@WebServlet("/doRegister")
+public class DoRegister extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoRegister() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		
+String customerId = request.getParameter("customerId");
+		
+		// Perform business logic. Return a been as a result
+		// CustomerService service = new CustomerService();
+		
+		 CustomerService service = (CustomerService) CustomerService.getInstance();
+		 
+		 System.out.println(service);
+		 
+		Customer customer = service.findCustomer(customerId);
+		
+		request.setAttribute("customer", customer);
+		
+		List<Customer> customerList = service.getAllCustomers();
+		request.setAttribute("customers", customerList);
+		
+		String page;
+		if(customer == null)
+			page ="/view/error.jsp";
+		else
+			page = "/view/registerSuccess.jsp";
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(page);
+		dispatcher.forward(request, response);
+	}
+
+}
